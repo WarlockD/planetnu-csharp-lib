f9330913288096da5e059134a23e36ddc5bee967
warlockd@gmail.com <warlockd@gmail.com@91a709ac-facc-a4e5-0af1-645f7fb5536e>
2012-03-18 08:44:50 +0000

    Switching to a branch before I screw myself

---
+++ branches/TypeSaftyJSON/PlanetNuLib/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace PlanetNuLib
+{
+    class Program
+    {
+        static void QuickSave(string filename, string s)
+        {
+            StreamWriter sw = new StreamWriter("games.json");
+             sw.Write(PlanetNu.GetGameList());
+            sw.Close();
+        }
+        static void Main(string[] args)
+        {
+            //StreamWriter sw = new StreamWriter("games.json");
+           // sw.Write(PlanetNu.GetGameList());
+            //sw.Close();
+            //StreamReader sw = new StreamReader("games.json");
+           // string s = sw.ReadToEnd();
+           // sw.Close();
+            string s = PlanetNu.GetTurnData(815, 3);
+            QuickSave("turn.json", s);
+            object test = JSON.JsonDecode(s);
+
+           // Console.WriteLine(PlanetNu.GetGameList());
+           // Console.WriteLine(key);
+            Console.WriteLine("End of Line");
+        }
+    }
+}
---
+++ branches/TypeSaftyJSON/PlanetNuLib/SimpleWeb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace PlanetNuLib
+{
+    class SimpleWeb
+    {
+        public SimpleWeb()
+        {
+            byte[] byteArray;
+            WebRequest request = WebRequest.Create("http://api.planets.nu/login");
+            request.Credentials = CredentialCache.DefaultCredentials;
+            //((HttpWebRequest)request).UserAgent = ".NET Framework Example Client";
+            request.Method = "POST";
+           // request.ContentLength = byteArray.Length;
+            request.ContentType = "application/x-www-form-urlencoded";
+            Stream dataStream = request.GetRequestStream();
+         //   dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            WebResponse response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+           // Stream data = response.GetResponseStream;
+            response.Close();
+        }
+    }
+}
